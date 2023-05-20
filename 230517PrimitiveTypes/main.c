4d1ff49ae964616a18d7993950d3fdb3260be29e
L8e9603 <L8e9603@gmail.com>
2023-05-20 20:08:23 +0900

    2023.05.20

---
+++ 230517PrimitiveTypes/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int int_value = 10;
+	long long_value = 100;
+	float float_value = 1.5f;
+	char char_value = 'A';
+
+	printf("int_value: %d\n", int_value);
+	printf("long_value: %ld\n", long_value);
+	printf("float_value: %d\n", char_value);
+	printf("char_value: %c\n", char_value);
+	
+	getch();
+
+	return 0;
+}
